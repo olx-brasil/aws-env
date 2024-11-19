2a3be9a9dac0899fef0f72530511c16b618428a8
Pedro Kiefer <pedro.kiefer@olxbr.com>
2024-11-19 18:04:24 -0300

    chore: add arm64

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NAME=aws-env
 for GOOS in darwin linux windows; do
-    for GOARCH in 386 amd64; do
+    for GOARCH in 386 amd64 arm64; do
         echo "Building $NAME-$GOOS-$GOARCH"
         GOOS=$GOOS GOARCH=$GOARCH go build -o $BUILD_DIR/$NAME-$GOOS-$GOARCH
     done
